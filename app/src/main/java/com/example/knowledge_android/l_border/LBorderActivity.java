406665c91a5ad07f7957109b67e31411c0cdd9fb
alex <jiahangsocket@126.com>
2021-10-17 23:11:40 +0800

    受教了LBorder

---
+++ app/src/main/java/com/example/knowledge_android/l_border/LBorderActivity.java
@@ -0,0 +1,16 @@
+package com.example.knowledge_android.l_border;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.knowledge_android.R;
+
+public class LBorderActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.l_border_layout);
+    }
+}
